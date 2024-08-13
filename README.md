8a036590a3c034e80b02fc2a0f4751e7256387fe
David Dal Busco <david.dalbusco@outlook.com>
2024-08-13 12:32:25 +0200

    feat: initial import

---
+++ README.md
@@ -0,0 +1,5 @@
+# Juno / Detect Features
+
+A website to detect browser's features. Quickly developed to try out Apple [Lockdown Mode](https://support.apple.com/en-us/105120).
+
+> 🧑‍🚀 Checkout [Juno](https://juno.build). Have fun!
---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Juno / Detect Features";
+export const SITE_DESCRIPTION = "Detect Features";
+export const SITE_SOCIAL_IMAGE =
+  "https://raw.githubusercontent.com/junobuild/create-juno/main/screenshots/screenshot-starter.png";
